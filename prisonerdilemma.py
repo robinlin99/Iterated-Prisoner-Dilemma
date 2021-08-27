27447ff1b2d34bbb3a5f125634b714b55e863fdd
Robin Lin <zl755@cornell.edu>
2021-08-26 21:24:52 -0700

    reformatted with pep8

--- prisonerdilemma.py
+++ prisonerdilemma.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import sqlite3
 
 # Standard Prisoner's Dilemma Payoff Matrix
+
+
 def evaluation(moveA, moveB):
     if moveA == 'c' and moveB == 'c':
         return 3, 3
@@ -17,6 +19,8 @@ def evaluation(moveA, moveB):
         return 0, 0
 
 # Generate score for Agent A and B, respectively, for a single round of Iterated Prisoner's Dilemma of length gene_length
+
+
 def generate_score(A, B, gene_length):
     ASum = 0
     BSum = 0
@@ -28,12 +32,15 @@ def generate_score(A, B, gene_length):
         BSum += BScore
     return ASum, BSum
 
-''' Higher-level Simulation Class 
 
+''' 
+Higher-level Simulation Class 
 
 '''
+
+
 class Simulation():
-    def __init__(self, pool_size, gene_length, totalrounds,predetermined):
+    def __init__(self, pool_size, gene_length, totalrounds, predetermined):
         self.poolsize = pool_size
         self.genesize = gene_length
         self.totalgenetic = 0
@@ -63,20 +70,21 @@ class Simulation():
         c = conn.cursor()
         c.execute('''CREATE TABLE players
              (strategy text, size real, bitstring text, id text, original text)''')
-        # Initialization 
+        # Initialization
         # Populating the reverse look up dictionary
-        for i in strategy_hashmap:  
+        for i in strategy_hashmap:
             self.reverse_lookup[strategy_hashmap[i]] = i
         if predetermined:
             # Adding new players to the simulation
             for strategy in strategy_hashmap:
                 if strategy in implemented:
                     for _ in range(self.poolsize):
-                        new = p.Player(strategy, 
+                        new = p.Player(strategy,
                                        self.genesize)
                         self.players.append(new)
                         # Add to database
-                        c.execute("INSERT INTO players VALUES ('{}', '{}', '{}', '{}', '{}')".format(strategy, self.genesize, new.strategy_bitstring, new.id, "True"))
+                        c.execute("INSERT INTO players VALUES ('{}', '{}', '{}', '{}', '{}')".format(
+                            strategy, self.genesize, new.strategy_bitstring, new.id, "True"))
                         conn.commit()
             # Creating tree object for each player
             for player in self.players:
@@ -85,8 +93,8 @@ class Simulation():
             # Populating initial genomic statistics
             for i in implemented:
                 self.totalgenetic += self.poolsize * self.genesize
-                self.genetic_instance[strategy_hashmap[i]] = self.poolsize * self.genesize
-
+                self.genetic_instance[strategy_hashmap[i]
+                                      ] = self.poolsize * self.genesize
 
     ''' 
     Simulation 1: 
@@ -102,7 +110,7 @@ class Simulation():
             - Overall simulation reflects a strictly-growth environment
     '''
 
-    def sim_configuration_1(self,rp):
+    def sim_configuration_1(self, rp):
         # Open database connection
         conn = sqlite3.connect('players.db')
         c = conn.cursor()
@@ -118,13 +126,14 @@ class Simulation():
             for left in range(0, len(self.players)-1):
                 for right in range(left+1, len(self.players)):
                     ARoundSum, BRoundSum = generate_score(
-                        self.players[left], 
-                        self.players[right], 
+                        self.players[left],
+                        self.players[right],
                         self.genesize)
                     playerA = self.players[left]
                     playerB = self.players[right]
                     print("Round: " + str(round))
-                    print(playerA.strategy_bitstring + " versus " + playerB.strategy_bitstring)
+                    print(playerA.strategy_bitstring +
+                          " versus " + playerB.strategy_bitstring)
                     self.game_hashmap[playerA] += ARoundSum
                     self.game_hashmap[playerB] += BRoundSum
             self.game_hashmap = {k: v for k, v in sorted(
@@ -142,11 +151,14 @@ class Simulation():
                 A = mates[0]
                 B = mates[1]
                 print("Round: " + str(round))
-                print("Mates: " + A.strategy_bitstring + ", " + B.strategy_bitstring)
+                print("Mates: " + A.strategy_bitstring +
+                      ", " + B.strategy_bitstring)
                 child1, child2 = A.crossover(B)
                 # Add to database
-                c.execute("INSERT INTO players VALUES ('{}', '{}', '{}', '{}', '{}')".format("N/A", child1.size, child1.strategy_bitstring, child1.id, "False"))
-                c.execute("INSERT INTO players VALUES ('{}', '{}', '{}', '{}', '{}')".format("N/A", child2.size, child2.strategy_bitstring, child2.id, "False"))
+                c.execute("INSERT INTO players VALUES ('{}', '{}', '{}', '{}', '{}')".format(
+                    "N/A", child1.size, child1.strategy_bitstring, child1.id, "False"))
+                c.execute("INSERT INTO players VALUES ('{}', '{}', '{}', '{}', '{}')".format(
+                    "N/A", child2.size, child2.strategy_bitstring, child2.id, "False"))
                 conn.commit()
                 for i in child1.strategy_bitstring:
                     self.genetic_instance[i] += 1
@@ -169,7 +181,7 @@ class Simulation():
                 self.game_hashmap[child1] = 0
                 self.game_hashmap[child2] = 0
             for i in self.game_hashmap:
-                print(i.strategy_bitstring,self.game_hashmap[i])
+                print(i.strategy_bitstring, self.game_hashmap[i])
             self.print_players()
         conn.close()
         self.plot_percentage_count()
@@ -177,13 +189,13 @@ class Simulation():
     def print_players(self):
         for player in self.players:
             print(player.id, player.strategy_bitstring)
-    
+
     def traverse_players(self):
         for player in self.players:
             print("New Traversal")
             print("----------------")
             self.tree_hashmap[player].inorder()
-    
+
     def process_percentage(self):
         for i in self.genetic_instance:
             percentage = float(self.genetic_instance[i]) / self.totalgenetic
@@ -192,7 +204,7 @@ class Simulation():
             else:
                 self.geneticpercentage[i].append(percentage)
             print(str(i) + ": " + str(percentage))
-    
+
     def process_count(self):
         for i in self.genetic_instance:
             count = self.genetic_instance[i]
@@ -201,45 +213,48 @@ class Simulation():
             else:
                 self.geneticcount[i].append(count)
             print(str(i) + ": " + str(count))
-    
+
     def generate_percentage_plot(self):
-        time = list(range(0,self.totalrounds))
+        time = list(range(0, self.totalrounds))
         for i in self.geneticpercentage:
             print(self.geneticpercentage[i])
             with plt.style.context('seaborn-darkgrid'):
-                plt.plot(time,self.geneticpercentage[i],'-o',label=self.reverse_lookup[i])
+                plt.plot(
+                    time, self.geneticpercentage[i], '-o', label=self.reverse_lookup[i])
 
         plt.xlabel("Epoch")
         plt.ylabel("Percentage of Total Population")
         plt.suptitle("Percentage of Total Population vs Epoch")
         plt.legend()
         plt.show()
-   
+
     def generate_number_plot(self):
-        time = list(range(0,self.totalrounds))
+        time = list(range(0, self.totalrounds))
         for i in self.geneticcount:
             print(self.geneticcount[i])
             with plt.style.context('seaborn-darkgrid'):
-                plt.plot(time,self.geneticcount[i],'-o',label=self.reverse_lookup[i],)
+                plt.plot(time, self.geneticcount[i],
+                         '-o', label=self.reverse_lookup[i],)
         plt.xlabel("Epoch")
         plt.ylabel("Agents")
         plt.legend("Agent Population vs Epoch")
         plt.show()
-    
+
     def plot_percentage_count(self):
         with plt.style.context('dark_background'):
             fig, (ax1, ax2) = plt.subplots(2)
             fig.suptitle(self.experimenttitle)
-        time = list(range(0,self.totalrounds))
+        time = list(range(0, self.totalrounds))
         for i in self.geneticcount:
             print(self.geneticcount[i])
-            ax1.plot(time,self.geneticcount[i],'-o',label=self.reverse_lookup[i],)
-        ax1.set(xlabel = "Epoch", ylabel = "Population")
+            ax1.plot(time, self.geneticcount[i],
+                     '-o', label=self.reverse_lookup[i],)
+        ax1.set(xlabel="Epoch", ylabel="Population")
         ax1.legend()
         for i in self.geneticpercentage:
             print(self.geneticpercentage[i])
-            ax2.plot(time,self.geneticpercentage[i],'-o',label=self.reverse_lookup[i])
-        ax2.set(xlabel = "Epoch", ylabel = "Population Percentage")
+            ax2.plot(
+                time, self.geneticpercentage[i], '-o', label=self.reverse_lookup[i])
+        ax2.set(xlabel="Epoch", ylabel="Population Percentage")
         ax2.legend()
         plt.show()
-
--- simTest.py
+++ simTest.py
@@ -4,24 +4,24 @@ import os
 import sqlite3
 
 
-
 class TestGame(unittest.TestCase):
     def test_sim(self):
-       os.system("rm players.db")
-       sim = prisonerdilemma.Simulation(4,20,7,True)
-       sim.sim_configuration_1(0.33)
-       sim.print_players()
-       sim.traverse_players()
-       def database_print():
-        # Create a SQL connection to our SQLite database
-        con = sqlite3.connect("players.db")
-        cur = con.cursor()
-        # The result of a "cursor.execute" can be iterated over by row
-        for row in cur.execute('SELECT * FROM players;'):
-            print(row)
-        # Be sure to close the connection
-        con.close()
-       database_print()
+        os.system("rm players.db")
+        sim = prisonerdilemma.Simulation(4, 20, 7, True)
+        sim.sim_configuration_1(0.33)
+        sim.print_players()
+        sim.traverse_players()
+
+        def database_print():
+            # Create a SQL connection to our SQLite database
+            con = sqlite3.connect("players.db")
+            cur = con.cursor()
+            # The result of a "cursor.execute" can be iterated over by row
+            for row in cur.execute('SELECT * FROM players;'):
+                print(row)
+            # Be sure to close the connection
+            con.close()
+        database_print()
 
 
 if __name__ == '__main__':
